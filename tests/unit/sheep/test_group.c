84e76c3b172074649409e7013ddc9b85c39b80ef
Satoshi Kuramochi <act.kura@gmail.com>
2016-06-21 19:20:33 +0900

    tests/unit: Add unit tests for sheep/group.c
    Add unit tests for sd_accept_handler()
    
    Signed-off-by: Yasuhito Fukuda <fukuda.yasuhito@po.ntts.co.jp>
    Signed-off-by: Satoshi Kuramochi <act.kura@gmail.com>

--- tests/unit/sheep/test_group.c
+++ tests/unit/sheep/test_group.c
@@ -11,25 +11,52 @@
 
 static void test_sd_accept_handler()
 {
-	struct sd_node joined;
-	struct rb_root nroot;
-	size_t nr_nodes;
-	struct cluster_info cinfo;
-	struct system_info __sys;
+	struct sd_node joined = {0};
+	struct rb_root nroot = {0};
+	size_t nr_nodes = 1;
+	struct cluster_info cinfo = {0};
+	struct system_info __sys = {0};
 
 	/* parameter set */
 	sys = &__sys;
 	__sys.this_node.nr_vnodes = 1;
+	__sys.vdi_inuse[0] = 9;
+	__sys.gateway_only = false;
+	__sys.cinfo.status = SD_STATUS_OK;
+	__sys.cinfo.flags |= SD_CLUSTER_FLAG_USE_LOCK;
+
 	cinfo.proto_ver = SD_SHEEP_PROTO_VER;
+	cinfo.epoch = 1;
+	cinfo.nr_nodes = 1;
+	cinfo.status = SD_STATUS_OK;
 	cinfo.ctime = 100;
+	cinfo.proto_ver = SD_SHEEP_PROTO_VER;
+
+	joined.nid.addr[12]=127; 
+	joined.nid.addr[13]=0; 
+	joined.nid.addr[14]=0; 
+	joined.nid.addr[15]=1; 
+	joined.nid.port=3;
 
 	/* create work queue */
 	init_event(EPOLL_SIZE);
 	init_work_queue(NULL);
 	sys->block_wqueue = create_ordered_work_queue("block");
 
+	/*
+	init_config_file();
+	*/
+	init_config_path(".");
+
+	/* to skip  call start_recovery() */
+	set_cluster_shutdown(true); 
+	set_cluster_config(&cinfo);
+
+	/* test target */
 	sd_accept_handler(&joined, &nroot, nr_nodes, &cinfo);
-	printf("sys->node_status = %d\n", __sys.node_status);
+
+	printf("\n");
+	printf("__sys.vdi_inuse[0]=%ld\n", __sys.vdi_inuse[0]);
 }
 
 int main(int argc, char **argv)
